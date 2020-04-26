048a1e793ca27b4b50a68b030156f289d8c2e8e1
Melih SAFRAN <32452019+melihsafran@users.noreply.github.com>
2020-04-26 15:53:15 +0300

    Add files via upload

---
+++ source/mStillWorkin/FormMain.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace mStillWorkin
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.statusStrip = new System.Windows.Forms.StatusStrip();
+            this.statusStripLblStatus = new System.Windows.Forms.ToolStripStatusLabel();
+            this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.statusStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // timer
+            // 
+            this.timer.Interval = 1000;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(12, 14);
+            this.btnStart.Margin = new System.Windows.Forms.Padding(0);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(126, 41);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Enabled = false;
+            this.btnStop.Location = new System.Drawing.Point(150, 14);
+            this.btnStop.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(126, 41);
+            this.btnStop.TabIndex = 3;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // statusStrip
+            // 
+            this.statusStrip.AutoSize = false;
+            this.statusStrip.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.statusStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.statusStripLblStatus});
+            this.statusStrip.Location = new System.Drawing.Point(0, 68);
+            this.statusStrip.Name = "statusStrip";
+            this.statusStrip.Padding = new System.Windows.Forms.Padding(1, 0, 16, 0);
+            this.statusStrip.Size = new System.Drawing.Size(289, 28);
+            this.statusStrip.SizingGrip = false;
+            this.statusStrip.TabIndex = 4;
+            // 
+            // statusStripLblStatus
+            // 
+            this.statusStripLblStatus.Name = "statusStripLblStatus";
+            this.statusStripLblStatus.Size = new System.Drawing.Size(79, 22);
+            this.statusStripLblStatus.Text = "    00:00:00";
+            // 
+            // notifyIcon
+            // 
+            this.notifyIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon.Icon")));
+            this.notifyIcon.Visible = true;
+            this.notifyIcon.Click += new System.EventHandler(this.notifyIcon_Click);
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(289, 96);
+            this.Controls.Add(this.statusStrip);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.MaximizeBox = false;
+            this.Name = "FormMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "mStillWorkin";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FormMain_FormClosing);
+            this.Load += new System.EventHandler(this.FormMain_Load);
+            this.Resize += new System.EventHandler(this.FormMain_Resize);
+            this.statusStrip.ResumeLayout(false);
+            this.statusStrip.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.StatusStrip statusStrip;
+        private System.Windows.Forms.ToolStripStatusLabel statusStripLblStatus;
+        private System.Windows.Forms.NotifyIcon notifyIcon;
+    }
+}
+
---
+++ source/mStillWorkin/FormMain.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace mStillWorkin
+{
+    public partial class FormMain : Form
+    {
+        #region Properties
+
+        public DateTime DateTimeStartWorking { get; set; }
+
+        INPUT[] inputs = new INPUT[1];
+
+        [DllImport("user32.dll", SetLastError = true)]
+        internal static extern uint SendInput(uint numberOfInputs, INPUT[] inputs, int sizeOftInputsArray);
+
+        #endregion
+
+        #region Events
+
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+            InitializeFormProperties();
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            SimulateWorking();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            StartTimer();
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            StopTimer();
+        }
+
+        private void FormMain_Resize(object sender, EventArgs e)
+        {
+            HideForm();
+        }
+
+        private void notifyIcon_Click(object sender, EventArgs e)
+        {
+            ShowForm();
+        }
+
+        private void FormMain_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            StopTimer();
+        }
+
+        #endregion
+
+        #region Methods
+
+        void InitializeFormProperties()
+        {
+            notifyIcon.Visible = true;
+
+            INPUT input = new INPUT
+            {
+                Type = 1
+            };
+            input.Data.Keyboard = new KEYBDINPUT();
+            input.Data.Keyboard.Vk = (ushort)KeyCode.CAPS_LOCK;
+            input.Data.Keyboard.Scan = 0;
+            input.Data.Keyboard.Flags = 2;
+            input.Data.Keyboard.Time = 0;
+            input.Data.Keyboard.ExtraInfo = IntPtr.Zero;
+
+            inputs[0] = input;
+        }
+
+        void StartTimer()
+        {
+            timer.Start();
+            DateTimeStartWorking = DateTime.Now;
+            ToggleButtons();
+        }
+
+        void StopTimer()
+        {
+            timer.Stop();
+            ToggleButtons();
+        }
+
+        void ToggleButtons()
+        {
+            btnStart.Enabled = btnStop.Enabled;
+            btnStop.Enabled = !btnStart.Enabled;
+        }
+
+        void SimulateWorking()
+        {
+            TimeSpan timeSpan = (DateTime.Now - DateTimeStartWorking);
+
+            string time = string.Format("{0:D2}:{1:D2}:{2:D2}", timeSpan.Hours, timeSpan.Minutes, timeSpan.Seconds);
+
+            statusStripLblStatus.Text = string.Format("    {0}", time);
+
+            try
+            {
+                if (SendInput(1, inputs, Marshal.SizeOf(typeof(INPUT))) == 0)
+                {
+                    throw new Exception();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(string.Format("{0}; {1}", ex.Message, ex.ToString()), string.Format("{0} Error", this.Text));
+            }
+        }
+
+        void HideForm()
+        {
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                Hide();
+            }
+        }
+
+        void ShowForm()
+        {
+            Show();
+            this.WindowState = FormWindowState.Normal;
+        }
+
+        #endregion
+
+
+    }
+}
